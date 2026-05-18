bf225ed9cf5c7b08c9bef6f76da9d31e808a7b80
carlos-brito <carlos.brito@hotmail.com>
2026-05-18 15:42:50 -0600

    ignore docker bind interface warning

--- app/main.py
+++ app/main.py
@@ -80,4 +80,4 @@ class WordResource(Resource):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)  # nosec B104
